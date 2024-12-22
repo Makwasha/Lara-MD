30a12114fcd7a615ab2831d1535d30d96fe6cbf5
Makwasha <makwashaedgar@gmail.com>
2024-12-22 15:24:41 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enhRTK4D#G478OX8_oVt7PnH9mwpzmFmQtu-XQ4tD_HfRYKyBJiU", // Add your session id
+SESSION_ID: process.env.SESSION_ID || " 2rQn2aTJ#0adm2W1iuBZDAhNBwGdOS4Sm7vy7S1vDEn6H12GrGYc", // Add your session id
 PREFIX: process.env.PREFIX || ".",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_STICKER: process.env.AUTO_STICKER || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 AUTO_REACT: process.env.AUTO_REACT || "false",
-FAKE_RECORDING: process.env.FAKE_RECORDING || "true",   
+FAKE_RECORDING: process.env.FAKE_RECORDING || "false",   
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",   
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
